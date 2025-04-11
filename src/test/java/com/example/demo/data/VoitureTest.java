c5b36c9bf2380c33b8f9bb579af40ff71358671c
12300720 <stepkowskimaxime@gmail.com>
2025-04-11 14:45:46 +0200

    Commit des tests

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -11,7 +11,10 @@ public class VoitureTest {
 
     @Test
     void creerVoiture(){
-        assertEquals(1,1);
+        Voiture voiture = new Voiture("McLaren",2_300_000);
+        assertEquals(2_300_000, voiture.getPrix());
+        Assert.isTrue("McLaren".equals(voiture.getMarque()), "La voiture doit être une McLaren P1");
     }
+
 
 }
