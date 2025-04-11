7666a8e99b52a135bffa0c0a1f9ef92a9c78acb8
12300720 <stepkowskimaxime@gmail.com>
2025-04-11 15:35:53 +0200

    Commit des tests Mockito

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -14,4 +14,26 @@ public class StatistiqueTests {
     @MockBean
     StatistiqueImpl statistiqueImpl;
 
+    @Test
+    void test_ajouter(){
+        doNothing().when(statistiqueImpl).ajouter(new Voiture("McLaren", 2_000_000));
+    }
+
+    @Test
+    void test_prix_moyen_throw() {
+        doThrow(ArithmeticException.class).when(statistiqueImpl).prixMoyen();
+    }
+
+    @Test
+    void test_prix_moyen_une_voiture() {
+        doNothing().when(statistiqueImpl).ajouter(new Voiture("Opel",2000));
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(1, 2000));
+    }
+
+    @Test
+    void test_prix_moyen_plusieurs_voitures() {
+        doNothing().when(statistiqueImpl).ajouter(new Voiture("Opel",2000));
+        doNothing().when(statistiqueImpl).ajouter(new Voiture("Mclaren",2_000_000));
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(2, 1_001_000));
+    }
 }
